46a32fd6f1c00051f072be5b1d296dda677cddbd
MaryamDost <maryam.dost@live.se>
2021-04-14 16:41:12 +0200

    Creating script to check RNA_raw data quality

---
+++ Code/02_RawReads_QualityControl_FastaQC.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+#SBATCH -A g2021012
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 01:00:00
+#SBATCH -J 02_RNA_rd_qc_fastqc
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user maryam.dost.3945@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load FastQC
+
+
+# running FastQC
+fastqc -t 4\
+-o ~/private/GenomeAnalysis/Data/
+~/private/GenomeAnalysis/Data/rawData/RNA_raw_data/ERR*.fastq.gz
