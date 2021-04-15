e106e30f8b90ec185a3d966f6ac260a77308d459
MaryamDost <60181533+MaryamDost@users.noreply.github.com>
2021-04-15 17:51:43 +0200

    Update 0102_assemblyQC_quast.sh

--- Code/0102_assemblyQC_quast.sh
+++ Code/0102_assemblyQC_quast.sh
@@ -5,7 +5,7 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 00:20:00
-#SBATCH -J 02_RNA_rd_qc_fastqc
+#SBATCH -J 02_quality_quast
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user maryam.dost.3945@student.uu.se
 
