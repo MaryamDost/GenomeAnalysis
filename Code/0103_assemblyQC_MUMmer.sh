2cb2715bb196f28ee3fd960d93c2f65c42ab5a58
MaryamDost <60181533+MaryamDost@users.noreply.github.com>
2021-04-15 22:03:29 +0200

    Update 0103_assemblyQC_MUMmer.sh

--- Code/0103_assemblyQC_MUMmer.sh
+++ Code/0103_assemblyQC_MUMmer.sh
@@ -4,8 +4,8 @@
 #SBATCH -M snowy
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 00:20:00
-#SBATCH -J 02_RNA_rd_qc_fastqc
+#SBATCH -t 00:30:00
+#SBATCH -J 02_RNA_rd_qc_Mummer
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user maryam.dost.3945@student.uu.se
 
