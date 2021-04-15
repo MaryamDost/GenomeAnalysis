823449ab65fb946a2decac317f36620b56a13199
MaryamDost <60181533+MaryamDost@users.noreply.github.com>
2021-04-15 23:36:46 +0200

    Update 0103_assemblyQC_MUMmer.sh

--- Code/0103_assemblyQC_MUMmer.sh
+++ Code/0103_assemblyQC_MUMmer.sh
@@ -13,25 +13,23 @@
 module load bioinfo-tools
 module load MUMmer/3.23
 
-# variables
-INPUT="~/private/GenomeAnalysis/Analyses/genomeAssembly/01_assembly_out/WGS_assembly.contigs.fasta"
-REF="~/private/GenomeAnalysis/Data/rawData/reference/OBMB01.fasta"
-MUMS="~/private/GenomeAnalysis/Analyses/genomeAssembly/0202_assemblyQC_MUMmer"
 
 # running MUMmer
-mummer -mum -b -c \
-$REF \ 
-$INPUT \
-> $MUMS/lferriphilum.mums
+# mummer -mum -b -c ref.fasta qry.fasta > ref_qry.mums
+mummer -mum -b -c ~/private/GenomeAnalysis/Data/rawData/reference/OBMB01.fasta ~/private/GenomeAnalysis/Analyses/genomeAssembly/01_assembly_out/WGS_assembly.contigs.fasta > lferriphilum.mums
+#i run the above on terminal, maybe thats why it worked, i need to give the whole path otherwise
+#~/private/GenomeAnalysis/Analyses/genomeAssembly/0202_assemblyQC_MUMmer/lferriphilum.mums
 
 # running mummerplot
-cd $MUMS
+#mummerplot --postscript --prefix=ref_qry ref_qry.mums
+#gnuplot ref_qry.gp
 
-mummerplot \
--Q $INPUT \
--R $REF \
-$MUMS/lferriphilum.mums \
---png --prefix=lferriphilum
-
+mummerplot --postscript --prefix=lferriphilum ~/private/GenomeAnalysis/Data/rawData/reference/OBMB01.fasta lferriphilum.mums
 gnuplot lferriphilum.gp
+
+#Ran again to get png file
+mummerplot \
+ --prefix=lferriphilum \
+ --png \
+ lferriphilum.mums
 
