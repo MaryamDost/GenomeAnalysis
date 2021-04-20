aacfae00be60d67e71ee218f6b211d7a5e507381
MaryamDost <60181533+MaryamDost@users.noreply.github.com>
2021-04-20 17:01:09 +0200

    Update 0102_assemblyQC_quast.sh

--- Code/0102_assemblyQC_quast.sh
+++ Code/0102_assemblyQC_quast.sh
@@ -13,9 +13,20 @@
 module load bioinfo-tools
 module load quast
 
+#
+# Input is the sequence file containd from the assembly and needs to be in fasta format. 
+# <input file path>
+#-R <path> referencer genome file
+#-o <path> -o <output_dir> Output directory
 
 #run quast
 quast.py \
 ~/private/GenomeAnalysis/Analyses/genomeAssembly/01_assembly_out/WGS_assembly.contigs.fasta \
 -R ~/private/GenomeAnalysis/Data/rawData/reference/OBMB01.fasta \
 -o ~/private/GenomeAnalysis/Analyses/genomeAssembly/0201AssemblyQC_quast
+
+# sbatch 0102_assemblyQC_quast.sh
+#this script takes the complite path, thus is ranable anywere in terminal.
+
+#-----------------------------------------------------------------------------------------------------------
+# More on how to code and run QUAST is found in its manual which is avalible in http://bioinformatics.se/tools/quast/manual.html
