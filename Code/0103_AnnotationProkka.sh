44f88bba78e5b641242280bb61f950901ea979af
MaryamDost <maryam.dost@live.se>
2021-04-22 00:13:58 +0200

    modified the named of the script to facilitate understanding of the steps and add the scrip for quality control of the trimmed data

---
+++ Code/0103_AnnotationProkka.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+#SBATCH -A g2021012
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 00:10:00
+#SBATCH -J 0104_Annotation_Prokka
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user maryam.dost.3945@student.uu.se
+
+#load modules 
+module load bioinfo-tools
+module load prokka/1.12-12547ca
+
+#--------- RUN PROKKA --------------------------------
+
+# input fasta file 
+# out dir 
+# prefix for files 
+
+prokka \
+ ~/private/GenomeAnalysis/Analyses/genomeAssembly/01_assembly_out/WGS_assembly.contigs.fasta
+ --outdir 0301Annotation_Prokka \
+ --prefix prokkaAnnotation 
---
+++ Code/0203_Trimdata_QualityControl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+#SBATCH -A g2021012
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 01:00:00
+#SBATCH -J 04__qctrimdata_fastqc
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user maryam.dost.3945@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load FastQC
+
+
+# running FastQC
+fastqc -t 2 \
+-o ~/private/GenomeAnalysis/Analyses/differentialExpressionAnalyses//0202_qc_TrimData \
+-d ~/private/GenomeAnalysis/Analyses/differentialExpressionAnalyses//0202_qc_TrimData \
+~/private/GenomeAnalysis/Data/rawData/2_Trimmomatic/*.fastq.gz
