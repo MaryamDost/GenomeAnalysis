adaf9f225df72b597e310b071f491676d8762501
MaryamDost <60181533+MaryamDost@users.noreply.github.com>
2021-04-28 12:12:16 +0200

    Update 0103_AnnotationProkka.sh

--- Code/0103_AnnotationProkka.sh
+++ Code/0103_AnnotationProkka.sh
@@ -21,5 +21,5 @@ module load prokka/1.12-12547ca
 prokka \
 # ~/private/GenomeAnalysis/Analyses/genomeAssembly/01_assembly_out/WGS_assembly.contigs.fasta
 ~/private/GenomeAnalysis/Analyses/genomeAssembly/01_assembly_out/lferriphilum_genome.fasta
---outdir ~/private/GenomeAnalysis/Analyses/genomeAssembly/0301Annotation_Prokkalferriphilum \
+--outdir 0301Annotation_Prokkalferriphilum \
 --prefix prokkaAnnotation_lferriphilum_genome
