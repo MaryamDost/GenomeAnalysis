86e723164fce80647f432ef5ee7802827d7847ca
MaryamDost <maryam.dost@live.se>
2021-04-29 17:10:38 +0200

    adding code for counting with Htseq

---
+++ Code/0205_counting_Htseq.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+#SBATCH -A g2021012
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 17:00:00
+#SBATCH -J 01_assembly_L.ferr_WGS
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user maryam.dost.3945@student.uu.se
+
+#Load modules
+module load bioinfo-tools
+module load htseq
+
+#-------------------- RUN Htseq ------------------------------
+
+
+ 
+# -f Format of the input data, here the format is in bam 
+# -r Alignment have to be sorted either by read name or by alignment position. 
+# -t feature 
+# -i id atribute, ID instead of gene_id in GFF file 
+# -s reverse due to paired-end reads, the first read has to be on the same strand and the second read on the opposite strand.
+# alignemnt files from bwa 
+# gff file from Anotated genome 
+ 
+path="~/private/GenomeAnalysis/Analyses/differentialExpressionAnalyses/03_mappingBWA"
+cd ~/private/GenomeAnalysis/Analyses/differentialExpressionAnalyses/04_countingHtseq
+
+for f in ERR2036629 ERR2036630 ERR2036631 ERR2036632 ERR2036633
+do
+htseq \
+    -count \
+    -f bam \
+    -r pos \
+    -t CDS \
+    -i ID \
+    -s reverse \
+    $path${f}.sorted.bam \
+    ~/private/GenomeAnalysis/Analyses/genomeAssembly/0301Annotation_ProkkaLferriphilum/Lferriphilum.gff \
+    > ${f}.txt
+
+    echo "done" 
+ done
+
+#-------------------------------------------------
